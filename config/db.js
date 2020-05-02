03ea06f7e29de5fdbd030e88336eccfa097985a6
Rob <betoarpi@gmail.com>
2020-05-01 19:45:24 -0500

    Se agregó la creación de usuarios

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+const conectarDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    });
+    console.log('Conexión a DB exitosa!')
+  } catch (error) {
+    console.log('Hubo un error');
+    console.log(error);
+    process.exit(1) // deter app
+  }
+}
+
+module.exports = conectarDB;
--- db/resolvers.js
+++ db/resolvers.js
@@ -1,7 +1,35 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+
 //Resolvers
 const resolvers = {
   Query: {
+    obtenerCurso: () => "Algo"
+  },
+  Mutation: {
+    nuevoUsuario: async (_, { input }) => {
+      const { email, password } = input;
 
+      //Revisar si el usuario ya existe en la base de datos
+      const existeUsuario = await Usuario.findOne({ email });
+      if (existeUsuario) {
+        throw new Error('El usuario ya está registrado');
+      }
+
+      //Hashear el password
+
+      const salt = await bcryptjs.genSalt(10);
+      input.password = await bcryptjs.hash(password, salt);
+
+      //Guardar en la base de datos
+      try {
+        const usuario = new Usuario(input);
+        usuario.save(); //guardarlo
+        return usuario;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 }
 
